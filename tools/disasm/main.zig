b254751f2f2b3a1d579d60e3e2a65df825ac540d
scemino <scemino74@gmail.com>
2024-08-05 13:55:35 +0200

    Replace writeDec in disasm

--- tools/disasm/main.zig
+++ tools/disasm/main.zig
@@ -47,22 +47,8 @@ pub fn Context(comptime ReaderType: type, comptime WriterType: type) type {
             self.write(buf2);
         }
 
-        // function to output an unsigned 8-bit value as decimal string
-        pub fn writeDec(self: *Self, val: u8) void {
-            if (val == 0) {
-                self.writer.writeByte(digits[0]) catch @panic("failed to write");
-                return;
-            }
-            var div: u8 = 100;
-            var b: bool = false;
-            inline for (0..3) |_| {
-                const v: u8 = (val / div) % 10;
-                if (b or (v > 0)) {
-                    b = true;
-                    self.writer.writeByte(digits[v & 0xF]) catch @panic("failed to write");
-                }
-                div /= 10;
-            }
+        fn writeInt(self: *Self, comptime T: type, value: T) void {
+            self.writer.print("{}", .{value}) catch @panic("format to write");
         }
 
         pub fn writeByte(self: *Self, val: u8) void {
@@ -81,27 +67,27 @@ pub fn disasmOp(pc: u16, reader: anytype, writer: anytype, context: anytype) u16
     switch (op) {
         0x00 => {
             ctx.write("set v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.writeByte(' ');
-            ctx.writeHex(ctx.fetch_u16()); // value
+            ctx.writeInt(i16, @bitCast(ctx.fetch_u16())); // value
         }, // mov const
         0x01 => {
             ctx.write("seti v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.write(" v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
         }, // mov
         0x02 => {
             ctx.write("addi v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.write(" v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
         }, // add
         0x03 => {
             ctx.write("addi v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.writeByte(' ');
-            ctx.writeHex(ctx.fetch_u16()); // value
+            ctx.writeInt(i16, @bitCast(ctx.fetch_u16())); // value
         }, // add const
         0x04 => {
             ctx.write("jsr ");
@@ -115,20 +101,20 @@ pub fn disasmOp(pc: u16, reader: anytype, writer: anytype, context: anytype) u16
         }, // jmp
         0x08 => {
             ctx.write("setvec ");
-            ctx.writeDec(ctx.fetch_u8()); // channel
+            ctx.writeInt(u8, ctx.fetch_u8()); // channel
             ctx.writeByte(' ');
             ctx.writeHex(ctx.fetch_u16()); // address
         }, // install task
         0x09 => {
             ctx.write("dbra v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.writeByte(' ');
             ctx.writeHex(ctx.fetch_u16()); // address
         }, // jmpIfVar
         0x0a => {
             const op2 = ctx.fetch_u8();
             ctx.write("si (v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             switch (op2 & 7) {
                 0 => ctx.write(" == "),
                 1 => ctx.write(" != "),
@@ -141,47 +127,46 @@ pub fn disasmOp(pc: u16, reader: anytype, writer: anytype, context: anytype) u16
             if ((op2 & 0x80) != 0) {
                 const a = ctx.fetch_u8();
                 ctx.write("v");
-                ctx.writeDec(a); // var
+                ctx.writeInt(u8, a); // var
             } else if ((op2 & 0x40) != 0) {
-                ctx.writeHex(ctx.fetch_u16()); // value
+                ctx.writeInt(i16, @bitCast(ctx.fetch_u16())); // value
             } else {
-                const a = ctx.fetch_u8();
-                ctx.writeHex(a); // var
+                ctx.writeInt(u8, ctx.fetch_u8());
             }
             ctx.write(") jmp ");
             ctx.writeHex(ctx.fetch_u16()); // address
         },
         0x0b => {
             ctx.write("fade ");
-            ctx.writeDec(@truncate(ctx.fetch_u16() >> 8)); // palette
+            ctx.writeInt(u8, @truncate(ctx.fetch_u16() >> 8)); // palette
         }, // setPalette
         0x0c => {
             ctx.write("vec ");
-            ctx.writeDec(ctx.fetch_u8()); // start
+            ctx.writeInt(u8, ctx.fetch_u8()); // start
             ctx.writeByte(',');
-            ctx.writeDec(ctx.fetch_u8()); // end
+            ctx.writeInt(u8, ctx.fetch_u8()); // end
             ctx.writeByte(',');
-            ctx.writeDec(ctx.fetch_u8()); // type
+            ctx.writeInt(u8, ctx.fetch_u8()); // type
         }, // changeTasksState
         0x0d => {
             ctx.write("setws ");
-            ctx.writeDec(ctx.fetch_u8()); // screen number
+            ctx.writeInt(u8, ctx.fetch_u8()); // screen number
         }, // selectPage
         0x0e => {
             ctx.write("clr ");
-            ctx.writeDec(ctx.fetch_u8()); // screen number
+            ctx.writeInt(u8, ctx.fetch_u8()); // screen number
             ctx.writeByte(' ');
-            ctx.writeDec(ctx.fetch_u8()); // color
+            ctx.writeInt(u8, ctx.fetch_u8()); // color
         }, // fillPage
         0x0f => {
             ctx.write("copy ");
-            ctx.writeDec(ctx.fetch_u8()); // screen number (src)
+            ctx.writeInt(u8, ctx.fetch_u8()); // screen number (src)
             ctx.writeByte(' ');
-            ctx.writeDec(ctx.fetch_u8()); // screen number (dst)
+            ctx.writeInt(u8, ctx.fetch_u8()); // screen number (dst)
         }, // copyPage
         0x10 => {
             ctx.write("show ");
-            ctx.writeDec(ctx.fetch_u8()); // screen number
+            ctx.writeInt(u8, ctx.fetch_u8()); // screen number
         }, // updateDisplay
         0x11 => ctx.write("bigend"), // removeTask
         0x12 => {
@@ -192,51 +177,51 @@ pub fn disasmOp(pc: u16, reader: anytype, writer: anytype, context: anytype) u16
             ctx.write("\" ");
             ctx.writeHex(text_num); // text number
             ctx.write(", ");
-            ctx.writeDec(ctx.fetch_u8()); // x
+            ctx.writeInt(u8, ctx.fetch_u8()); // x
             ctx.write(", ");
-            ctx.writeDec(ctx.fetch_u8()); // y
+            ctx.writeInt(u8, ctx.fetch_u8()); // y
             ctx.write(", ");
-            ctx.writeDec(ctx.fetch_u8()); // color
+            ctx.writeInt(u8, ctx.fetch_u8()); // color
         }, // text "text number", x, y, color
         0x13 => {
             ctx.write("sub v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.write(", v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
         }, // sub
         0x14 => {
             ctx.write("andi v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.write(", ");
-            ctx.writeHex(ctx.fetch_u16()); // value
+            ctx.writeInt(i16, @bitCast(ctx.fetch_u16())); // value
         }, // and
         0x15 => {
             ctx.write("ori v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.write(", ");
-            ctx.writeHex(ctx.fetch_u16()); // value
+            ctx.writeInt(i16, @bitCast(ctx.fetch_u16())); // value
         }, // or
         0x16 => {
             ctx.write("shl v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.write(", ");
-            ctx.writeHex(ctx.fetch_u16()); // value
+            ctx.writeInt(i16, @bitCast(ctx.fetch_u16())); // value
         }, // shl
         0x17 => {
             ctx.write("shr v");
-            ctx.writeDec(ctx.fetch_u8()); // var
+            ctx.writeInt(u8, ctx.fetch_u8()); // var
             ctx.write(", ");
-            ctx.writeHex(ctx.fetch_u16()); // value
+            ctx.writeInt(i16, @bitCast(ctx.fetch_u16())); // value
         }, // shr
         0x18 => {
             ctx.write("play ");
             ctx.writeHex(ctx.fetch_u16()); // file num
             ctx.writeByte(' ');
-            ctx.writeDec(ctx.fetch_u8()); // note
+            ctx.writeInt(u8, ctx.fetch_u8()); // note
             ctx.write(", ");
-            ctx.writeDec(ctx.fetch_u8()); // volume
+            ctx.writeInt(u8, ctx.fetch_u8()); // volume
             ctx.write(", ");
-            ctx.writeDec(ctx.fetch_u8()); // channel
+            ctx.writeInt(u8, ctx.fetch_u8()); // channel
         }, // playSound
         0x19 => {
             ctx.write("load ");
@@ -246,9 +231,9 @@ pub fn disasmOp(pc: u16, reader: anytype, writer: anytype, context: anytype) u16
             ctx.write("song ");
             ctx.writeHex(ctx.fetch_u16()); // file num
             ctx.writeByte(' ');
-            ctx.writeHex(ctx.fetch_u16()); // tempo
+            ctx.writeInt(u16, ctx.fetch_u16()); // tempo
             ctx.writeByte(' ');
-            ctx.writeDec(ctx.fetch_u8()); // pos
+            ctx.writeInt(u8, ctx.fetch_u8()); // pos
         }, // playMusic
         else => {
             if ((op & 0x80) != 0) {
@@ -256,9 +241,9 @@ pub fn disasmOp(pc: u16, reader: anytype, writer: anytype, context: anytype) u16
                 ctx.write("spr ");
                 ctx.writeHex(off);
                 ctx.writeByte(' ');
-                ctx.writeDec(ctx.fetch_u8());
+                ctx.writeInt(u8, ctx.fetch_u8());
                 ctx.write(" ");
-                ctx.writeDec(ctx.fetch_u8());
+                ctx.writeInt(u8, ctx.fetch_u8());
                 ctx.writeByte(' ');
             } else if ((op & 0x40) != 0) {
                 const off: u16 = ((@as(u16, @intCast(ctx.fetch_u8())) << 8) | ctx.fetch_u8()) << 1;
@@ -270,7 +255,7 @@ pub fn disasmOp(pc: u16, reader: anytype, writer: anytype, context: anytype) u16
                         ctx.writeHex(ctx.fetch_u16());
                     } else {
                         ctx.write("v");
-                        ctx.writeDec(ctx.fetch_u8());
+                        ctx.writeInt(u8, ctx.fetch_u8());
                     }
                 } else {
                     var x: i16 = ctx.fetch_u8();
@@ -286,22 +271,22 @@ pub fn disasmOp(pc: u16, reader: anytype, writer: anytype, context: anytype) u16
                         ctx.writeHex(@bitCast(y));
                     } else {
                         ctx.write("v");
-                        ctx.writeDec(ctx.fetch_u8());
+                        ctx.writeInt(u8, ctx.fetch_u8());
                     }
                 } else {
-                    ctx.writeDec(ctx.fetch_u8());
+                    ctx.writeInt(u8, ctx.fetch_u8());
                 }
                 ctx.write(" ");
                 if ((op & 2) == 0) {
                     if ((op & 1) != 0) {
                         ctx.write("v");
-                        ctx.writeDec(ctx.fetch_u8());
+                        ctx.writeInt(u8, ctx.fetch_u8());
                     } else {
                         ctx.write("64");
                     }
                 } else {
                     if ((op & 1) == 0) {
-                        ctx.writeDec(ctx.fetch_u8());
+                        ctx.writeInt(u8, ctx.fetch_u8());
                     } else {
                         ctx.write("64");
                     }
